c63496f2ab37311912230af5cb5b40c7539b47aa
Dominik Smith <smithdominik97@gmail.com>
2023-10-31 17:04:24 +0000

    added css styling and created api.tsx for db methods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
 'use client'
 import { useEffect } from 'react';
-import returnDatabase from "../db/database";
+import getDatabase from "../db/connection";
+import { insertAsset } from '@/db/api';
+import { getAsset } from '@/db/api';
 import { invoke } from '@tauri-apps/api';
+import styles from './page.module.css';
 
 
 
@@ -13,12 +16,52 @@ export default function Home() {
     }
   }, []);
 
-  
-  returnDatabase();
+  getAsset();
+  // insertAsset();
+ 
 
   return (
     <>
-      <h1>Hello</h1>
+      <header className={styles.header}>
+      <nav>
+        <ul className={styles.navbar}>
+          <li className={styles.navitem}>
+            <p>System info</p>
+          </li>
+
+          <li className={styles.navitem}>
+            <p>User: <span>Dominik</span></p>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <div className={styles.assetdisplay}>
+        <button className={styles.addbtn}><span>Add</span></button>
+
+        <div className={styles.assetitem}>
+          <div className={styles.assetbuttons}>
+            <button type="button" className={`${styles.btnitem} ${styles.deletebtn}`}>
+              <span>Delete</span>
+            </button>
+            <button type="button" className={`${styles.btnitem} ${styles.editbtn}`}>
+              <span>Edit</span>
+            </button>
+          </div>
+
+          <div className={styles.assetinfo}>
+            <h1>System Name <span></span></h1>
+            <ul>
+              <li>Model: <span></span></li>
+              <li>Manufacturer: <span></span></li>
+              <li>Type: <span></span></li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </main>
     </>
   );
 }
---
+++ src/db/connection.tsx
@@ -0,0 +1,31 @@
+import Database from "tauri-plugin-sql-api";
+
+
+
+
+export default async function getDatabase() {
+  const db = await Database.load(
+    "mysql://sql2207064:factor%20locks%20devon%20cars@lochnagar.abertay.ac.uk/sql2207064"
+  );
+
+  if (!db.execute) {
+    console.log("Failed to connect to database");
+    throw new Error("Falled to connect to database");
+  }
+  console.log("Connected to database");
+
+  // const select = await db.select(
+  //   "SELECT * FROM employee"
+  // ) as { eno: number, ename: string, edepartment: string, ejob: string, ephone: string, eroom: string, eemail: string }[];
+
+  // for (const row of select) {
+  //   console.log(row);
+  // }
+
+  return db;
+}
+
+// export default async function returnDatabase(){
+//   const result = await getDatabase();
+//   console.log(result);
+// }
