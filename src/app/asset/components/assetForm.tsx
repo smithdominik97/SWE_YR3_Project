8fe18111a46aafb612dee709ef2fcfcd94d5dcd7
Dominik Smith <smithdominik97@gmail.com>
2023-12-16 18:47:10 +0000

    added functionality to insert system info into automatically into form

--- src/app/asset/components/assetForm.tsx
+++ src/app/asset/components/assetForm.tsx
@@ -1,4 +1,5 @@
 import { InsertAsset } from "@/api/api"
+import React, { useState } from 'react';
 import { Asset } from "@/app/asset/components/assetItem"
 import styles from "./form.module.css"
 import  returnSysinfo  from "@/controller/sysinfo"
@@ -8,6 +9,8 @@ interface AssetFormProps {
   }
 
 export default function AssetForm({ onSubmit }: AssetFormProps)  {
+
+    
 
     const assetsubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -27,29 +30,47 @@ export default function AssetForm({ onSubmit }: AssetFormProps)  {
         onSubmit();
     }
 
+    const [systemname, setSystemname] = useState('');
+    const [model, setModel] = useState('');
+    const [memory, setMemory] = useState('');
+    const [os, setOs] = useState('');
+    const [kernel, setKernel] = useState('');
+    const [type, setType] = useState('');
+    const [ip, setIp] = useState('');
 
-    const getSysInfo = async (event: React.FormEvent<HTMLFormElement>) => {
+
+
+    const getSysInfo = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
-        const formData = new FormData(form);
 
         const sysInfo = await returnSysinfo();
         console.log(sysInfo);
+
+        setSystemname(sysInfo.hostname);
+        setModel(sysInfo.cpu);
+        setMemory(sysInfo.memory.toString());
+        setOs(sysInfo.os);
+        setKernel(sysInfo.kernelVersion);
+        setType(sysInfo.systemType);
+        setIp(sysInfo.ipAddress);
     }
 
     return (
         <form method="POST" onSubmit={assetsubmit} className={styles.form}>
             <label htmlFor="systemname">System Name: </label>
-            <input type="text" name="systemname"/>
-            <label htmlFor="CPU Model">Model: </label>
-            <input type="text" name="model"/>
+            <input type="text" name="systemname" value={systemname}/>
+            <label htmlFor="model">CPU Model: </label>
+            <input type="text" name="model" value={model}/>
+            <label htmlFor="memory">Memory (GB): </label>
+            <input type="number" name="memory" value={memory}/>
             <label htmlFor="os">Operating System: </label>
-            <input type="text" name="os"/>
+            <input type="text" name="os" value={os}/>
             <label htmlFor="kernel">Kernel Version: </label>
-            <input type="text" name="kernel"/>
+            <input type="text" name="kernel" value={kernel}/>
             <label htmlFor="type">System type: </label>
-            <input type="text" name="type"/>
+            <input type="text" name="type" value={type}/>
             <label htmlFor="ip">IP Address: </label>
-            <input type="text" name="ip"/>
+            <input type="text" name="ip" value={ip}/>
             <label htmlFor="purchasedate">Purchase Date: </label>
             <input type="text" name="purchasedate"/>
             <label htmlFor="note">Note: </label>
